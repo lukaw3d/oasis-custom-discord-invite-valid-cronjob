43782f363091bd93137be886bc11d459cb3ae4ae
Luka Jeran <lukaw3d@gmail.com>
2023-07-10 15:42:01 +0200

    Nodejs script to check

---
+++ check.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+// https://oasis.io/discord
+// https://discord.com/invite/BQCxwhT5wS
+
+// https://oasis.io/discord/invite-api-check
+// https://discord.com/api/v9/invites/BQCxwhT5wS
+
+import assert from 'node:assert/strict'
+
+const inviteRequest = await fetch('https://oasis.io/discord')
+assert(
+  inviteRequest.url.startsWith('https://discord.com/invite/'),
+  'Custom discord link "https://oasis.io/discord" still redirects',
+)
+
+const inviteApiCheckUrl = inviteRequest.url.replace(
+  'https://discord.com/invite/',
+  'https://discord.com/api/v9/invites/',
+)
+const inviteApiCheckRequest = await fetch('https://oasis.io/discord/invite-api-check')
+assert.equal(
+  inviteApiCheckRequest.url, inviteApiCheckUrl,
+  'Discord API check link "https://oasis.io/discord/invite-api-check" still matches invite link "https://oasis.io/discord"',
+)
+assert.equal(inviteApiCheckRequest.status, 200, 'Discord invite link check is valid')
